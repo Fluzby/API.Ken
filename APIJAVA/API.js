48c2abd1b1a6a1428946242255a5a348acd0fc99
Fluzby <147047646+Fluzby@users.noreply.github.com>
2024-09-09 11:46:06 +0300

    Add files via upload

--- APIJAVA/API.js
+++ APIJAVA/API.js
@@ -1,6 +1,8 @@
 const randonmJokeHTMLElement = document.querySelector('.random-jokes');
 const selectHTMLElement = document.querySelector('#categories')
 const buttonElement = document.querySelector('.generate-joke-button')
+const searchElement = document.querySelector('#search')
+const searchResultsWrapper = document.querySelector('.search-results')
 
 const base_url = "https://api.chucknorris.io/jokes"
 let selectedOption = null;
@@ -53,6 +55,24 @@ buttonElement.addEventListener('click', async (event) => {
     const response = await fetchRandomJokes(selectedOption)
     randonmJokeHTMLElement.textContent = response.value
 })
+
+searchElement.addEventListener('click', async (event) => {
+    if (event.currentTarget.value.length < 3)  return
+
+    const response = await searchQuery(event.currentTarget.value)
+
+    const resultCountPlural = response.total === 1 ? 'nali' : 'nalja'
+    resultCountWrapper.innerText = `Leitud${response.total} nalja.`
+})
+
+const searchQuery = async (query) => {
+    const url = `${base_url}/search?query=${query}`
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return(data)
+}
 
 displayRandomJoke()
 fetchCategories()
